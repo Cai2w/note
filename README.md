cc477a3a9771d6a67c8ddf3c1c3f91ace990792a
Cai2w <1732804469@qq.com>
2024-09-11 20:39:55 +0800

    初始化仓库

---
+++ README.md
@@ -0,0 +1,4 @@
+# 本站地址
+
+[https://cmty256.github.io/](https://cmty256.github.io/)
+
---
+++ docs/.vuepress/config/nav.js
@@ -0,0 +1,103 @@
+module.exports = [
+  { text: "首页", link: "/" },
+  {
+    text: "Java",
+    items: [
+      { text: "基础篇", link: "/pages/11aacc/" },
+      { text: "集合篇", link: "/pages/2e780e/" },
+      { text: "并发篇", link: "/pages/846d88/" },
+      { text: "JVM", link: "/pages/562a37/" },
+      { text: "Java8 新特性", link: "/pages/a5d9a6/" },
+    ]
+  },
+  {
+    text: "计算机基础",
+    items: [
+      { text: "计算机网络", link: "/pages/7b1cb2/" },
+      { text: "操作系统", link: "/pages/6048a8/" },
+      { text: "数据结构与算法", link: "/pages/3b34ba/" }
+    ]
+  },
+  {
+    text: "数据库",
+    items: [
+      { text: "基础篇", link: "/pages/efa3f2/" },
+      { text: "MySql", link: "/pages/ccc445/" },
+      { text: "Redis", link: "/pages/54616e/" },
+      { text: "达梦", link: "/pages/980c9c/" },
+    ]
+  },
+  {
+    text: "常用框架",
+    items: [
+      { text: "Spring", link: "/pages/4969e6/" },
+      { text: "SpringBoot", link: "/pages/72bcc7/" },
+      { text: "Mybatis", link: "/pages/93b888/" },
+      { text: "Shiro", link: "/pages/a54fbc/" },
+    ]
+  },
+  {
+    text: "系统设计",
+    items: [
+      { text: "设计须知", link: "/pages/649916/" },
+      { text: "UML画图", link: "/pages/3fcc71/" },
+      { text: "权限校验", link: "/pages/793d0a/" },
+      { text: "设计模式", link: "/pages/6ea7eb/" },
+    ]
+  },
+  {
+    text: "微服务",
+    items: [
+      { text: "API网关", link: "/pages/c6222f/" },
+      { text: "RPC", link: "/pages//" },
+      { text: "消息队列", link: "/pages/2f66e5/" },
+      { text: "SpringCloud", link: "/pages/75e0cf/" },
+      { text: "分布式事务", link: "/pages/957695/" },
+      { text: "云存储", link: "/pages/2beec7/" },
+      { text: "虚拟机", link: "/pages/412fe7/" },
+    ]
+  },
+  {
+    text: '开发日常',
+    items: [
+        { text: "开发工具篇", link: "/pages/626493/" },
+        { text: "工具库篇", link: "/pages/bdbd1f/" },
+        { text: "开发技巧篇", link: "/pages/cc202d/" },
+        { text: "工具类系列", link: "/pages/dea09d/" },
+        { text: "随笔", link: "/pages/3cd6be/" },
+    ]
+  },
+  {
+    text: '前端学习',
+    items: [
+      { text: "HTML与CSS", link: "/pages/99897f/" },
+      { text: "JS学习", link: "/pages/51542d/" },
+      { text: "Vue3入门", link: "/pages/803f9d/" },
+      { text: "Vue3进阶", link: "/pages/ca4cfb/" },
+      { text: "黑马Vue3", link: "/pages/50e8d3/" },
+    ]
+  },
+  {
+    text: '项目笔记',
+    items: [
+      { text: "脚手架搭建", link: "/pages/ebb9f9/" },
+      { text: "瑞吉外卖", link: "/pages/fbc2fc/" },
+      { text: "黑马点评", link: "/pages/ee76ba/" },
+      { text: "vue-blog", link: "/pages/fa01b6/" },
+      { text: "沉梦接口开放平台", link: "/pages/b363e2/" },
+      { text: "用户中心", link: "/pages/72b962/" },
+      { text: "聚合搜索平台", link: "/pages/8baa93/" },
+      { text: "仿12306项目", link: "/pages/a6b2f3/" },
+      { text: "RuoYi-Vue", link: "/pages/a2c11e/" },
+    ]
+  },
+  {
+    text: '其它',
+    items: [
+      { text: "博客搭建", link: "/pages/fa256e/" },
+      { text: "网站收藏箱", link: "/pages/335531/" },
+      { text: "断墨寻径摘录", link: "/pages/94109b/" },
+      { text: "费曼学习法", link: "/pages/22f2c8/" },
+    ]
+  }
+];
